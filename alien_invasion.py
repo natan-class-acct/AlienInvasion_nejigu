e1d8c6d619ee57a4399fa24fd38a471988bb7431
Natan Ejigu <natanejigit@gmail.com>
2026-08-02 12:36:28 -0400

    adding bullet-alien collision detection

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,6 +90,10 @@ class AlienInvasion:
             if bullet.rect.top > self.settings.screen_height:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens and if that is the case,  get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
