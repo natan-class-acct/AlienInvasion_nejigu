40b67a45ffcaae4d2597df1329932d86ceece8de
Natan Ejigu <natanejigit@gmail.com>
2026-08-02 12:49:15 -0400

    pusshing refactrore bullet-a;ien collision into separet method

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,8 +91,16 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
         # Check for any bullets that have hit aliens and if that is the case,  get rid of the bullet and the alien.
+        self._check_bullet_alien_collisions()
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisionsand and remove any bullets and aliens that have collided."""
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+ 
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
