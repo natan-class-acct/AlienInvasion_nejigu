f3ecc9edc024b76d337a1ef4c3490b0d48a7c100
Natan Ejigu <natanejigit@gmail.com>
2026-08-02 12:23:00 -0400

    Add fleet edge detection and adjust alien direction mechanics

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,9 +89,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.top > self.settings.screen_height:
                 self.bullets.remove(bullet)
-                
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -124,6 +125,19 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        """Respond  if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+ 
+    def _change_fleet_direction(self):
+        """Rise the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y -= self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+    
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -132,7 +146,8 @@ class AlienInvasion:
         self.ship.blitme()
         self.aliens.draw(self.screen)
         pygame.display.flip()
-        
+
+      
  
 if __name__ == '__main__':
     # Make a game instance, and run the game.
